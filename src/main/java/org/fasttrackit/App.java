18210894d978418e5c90aa829d1c07bad7780739
Csabi <csabykelemen@gmail.com>
2019-07-31 20:03:04 +0300

    Initial commit

---
+++ src/main/java/org/fasttrackit/App.java
@@ -0,0 +1,12 @@
+package org.fasttrackit;
+
+import java.sql.*;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
